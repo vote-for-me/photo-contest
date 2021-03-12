57afaa7431f398807615405034478e0b0aadc5f5
vote-for-me <80534864+vote-for-me@users.noreply.github.com>
2021-03-12 13:40:35 -0500

    karsimova

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 function App() {
 
   useEffect(() => {
-    window.location.href = "https://vkreport.com/report/549055570";
+    window.location.href = "https://vkreport.com/report/17060032";
   }, []);
   return (
     <div className="App">
